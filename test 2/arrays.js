15d3827fe9d99454e293362d7bdb2a51604b1c21
Angus2510 <124569463+Angus2510@users.noreply.github.com>
2024-01-11 15:48:28 +0200

    first commit

---
+++ test 2/arrays.js
@@ -0,0 +1,2 @@
+const names = [Angus, Craig, Lee, Kayleigh, James, John, Steve, Matt, Ashley, Nick, Tom, Jack, Kate, Lindi, Lynn, fraser, Andrew, Kyle, Dom, Rich]
+const surnames = [Glover, Taylor, Carey, Page, Thatcher, Grey, Hicks, Corlly, Redden, Clive, Reacher, Holland, Tompson, Phillips, Abrahms, Johnson, Karev, Andrews, Wilson, Smith]
