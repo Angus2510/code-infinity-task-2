ba56a07e86e645fc70293ede5e644005b033414c
Angus2510 <124569463+Angus2510@users.noreply.github.com>
2024-01-12 15:29:37 +0200

    second commit

--- test 2/arrays.js
+++ test 2/arrays.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 const names = [
   Angus,
   Craig,
@@ -62,7 +64,7 @@ function generatePeople(amountOfPeople) {
   for (let i = 0; i < amountOfPeople; i++) {
     const firstName = names[Math.floor(Math.random() * names.length)];
     const lastName = surnames[Math.floor(Math.random() * surnames.length)];
-    const initials = firstName.at(0);
+    const initials = firstName[0];
     const birthday = generateBirthDay();
     const age = new Date().getFullYear() - new Date(birthday).getFullYear();
     csvData += `${firstName}, ${lastName}, ${initials}, ${age}, ${birthday}`;
@@ -70,3 +72,38 @@ function generatePeople(amountOfPeople) {
 
   return csvData;
 }
+
+function jsonToCsv(items) {
+  const header = Object.keys(items[0]);
+  const headerString = header.join(",");
+  // handle null or undefined values here
+  const replacer = (key, value) => value ?? "";
+  const rowItems = items.map((row) =>
+    header
+      .map((fieldName) => JSON.stringify(row[fieldName], replacer))
+      .join(",")
+  );
+  // join header and body, and break into separate lines
+  const csv = [headerString, ...rowItems].join("\r\n");
+  return csv;
+}
+
+const generatedData = generatePeople(10);
+
+const rows = generatedData
+  .split("\n")
+  .splice(1)
+  .map((row) => row.split(","))
+  .map(([firstName, lastName, initials, age, birthday]) => ({
+    firstName,
+    lastName,
+    initials,
+    age,
+    birthday,
+  }));
+
+const csvContent = jsonToCsv(rows);
+
+fs.writeFileSync("people.csv", csvContent, "utf-8");
+
+console.log("CSV data has been saved to people.csv");
